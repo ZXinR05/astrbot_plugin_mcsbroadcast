47c1077983b02257f84b2d9656beba64ae0a051e
XIN <qq2484199851@163.com>
2025-04-19 14:50:17 +0800

    fix bug

--- utils/event_handler.py
+++ utils/event_handler.py
@@ -10,10 +10,10 @@ def event_handel(data, sid):
     }
     event = data["event"]
     if event == "player_death":
-        message["content"] = f"{data.get("player")}死了:{data.get("cause")}"
+        message["content"] = f'{data.get("player")}死了:{data.get("cause")}'
 
     if event == "player_chat":
-        message["content"] = f"{data.get("player")}说:{data.get("message")}"
+        message["content"] = f'{data.get("player")}说:{data.get("message")}'
 
     if event == "server_start":
         message["content"] = "服务器已启动"
@@ -22,13 +22,13 @@ def event_handel(data, sid):
         message["content"] = "服务器已关闭"
     
     if event == "player_login":
-        message["content"] = f"{data.get("player")}加入了游戏"
+        message["content"] = f'{data.get("player")}加入了游戏'
 
     if event == "player_quit":
-        message["content"] = f"{data.get("player")}退出了游戏"
+        message["content"] = f'{data.get("player")}退出了游戏'
 
     if event == "player_respawn":
-        message["content"] = f"{data.get("player")}重生了"
+        message["content"] = f'{data.get("player")}重生了'
 
     if event == "weather_change":
         message["content"] = "我的世界下雨了" if data.get("rain") else "我的世界又不下雨了"
